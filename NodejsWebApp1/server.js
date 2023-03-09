82ee84b2366ea7d74406bf831a746f4086f1b555
Razvan Gherman <r.gherman@levi9.com>
2023-03-09 10:30:34 +0200

    POST 'books': success response message 'Books with title 'title 1' submitted'

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -2,7 +2,7 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 const host = 'localhost'; 
-var qs = require("querystring");
+const { parse } = require('querystring');
 
 const books = JSON.stringify([
     { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
@@ -44,6 +44,7 @@ const requestListener = function (req, res) {
 const server = http.createServer(requestListener)
 
 server.listen(port);
+
 console.log("Use browser to get url 'http://localhost:" + port +"'");
 console.log("Available endpoints:");
 console.log("    '/books'");
@@ -54,70 +55,18 @@ function get405(req, resp) {
     //resp.writeHead(405, "Method not supported", { "Content-Type": "text/html" });
     //resp.write("<html><html><head><title>405</title></head><body>405: Method not supported</body></html>");
     //resp.end();
-    resp.writeHead(405);
+    
+	resp.writeHead(405);
     resp.end(JSON.stringify({ error: "Method not supported" }));
 }
 
 function get404(req, resp) {
     //resp.writeHead(404, "Resource Not Found", { "Content-Type": "text/html" });
-    //resp.write("<html><html><head><title>404</title></head><body>404: Resource not found. Go to <a href='/'>Home</a></body></html>");
+    //resp.write("<html><html><head><title>405</title></head><body>404: Resource Not Found</body></html>");
     //resp.end();
-    resp.writeHead(404);
+    
+	resp.writeHead(404);
     resp.end(JSON.stringify({ error: "Resource not found" }));
-}
-
-function getCalcHtml(req, resp, data) {
-    var sb = new StringBuilder({ newline: "\r\n" });
-    sb.appendLine("<html>");
-    sb.appendLine(" <body>");
-    sb.appendLine("     <form method='post'>");
-    sb.appendLine("         <table>");
-    sb.appendLine("             <tr>");
-    sb.appendLine("                 <td>Enter First No: </td>");
-
-    if (data && data.txtFirstNo) {
-        sb.appendLine("                 <td><input type='text' id='txtFirstNo' name='txtFirstNo' value='{0}'/></td>", data.txtFirstNo);
-    }
-    else {
-        sb.appendLine("                 <td><input type='text' id='txtFirstNo' name='txtFirstNo' /></td>");
-    }
-
-    sb.appendLine("             </tr>");
-    sb.appendLine("             <tr>");
-    sb.appendLine("                 <td>Enter Second No: </td>");
-
-    if (data && data.txtSecondNo) {
-        sb.appendLine("                 <td><input type='text' id='txtSecondNo' name='txtSecondNo' value='{0}'/></td>", data.txtSecondNo);
-    }
-    else {
-        sb.appendLine("                 <td><input type='text' id='txtSecondNo' name='txtSecondNo' /></td>");
-    }
-
-    sb.appendLine("             </tr>");
-    sb.appendLine("             <tr>");
-    sb.appendLine("                 <td><input type='submit' value='Calculate' /></td>");
-    sb.appendLine("             </tr>");
-
-    if (data && data.txtFirstNo && data.txtSecondNo) {
-        var sum = parseInt(data.txtFirstNo) + parseInt(data.txtSecondNo);
-        sb.appendLine("             <tr>");
-        sb.appendLine("                 <td>Sum: {0}</td>", sum);
-        sb.appendLine("             </tr>");
-    }
-
-    sb.appendLine("         </table>");
-    sb.appendLine("     </form>")
-    sb.appendLine(" </body>");
-    sb.appendLine("</html>");
-    sb.build(function (err, result) {
-        resp.write(result);
-        resp.end();
-    });
-}
-
-function getCalcForm(req, resp, data) {
-    resp.writeHead(200, { "Content-Type": "text/html" });
-    getCalcHtml(req, resp, data);
 }
 
 function ProcessGet(req, res) {
@@ -147,24 +96,26 @@ function ProcessGet(req, res) {
 function ProcessPost(req, res) {
     switch (req.url) {
         case "/books":
-            var reqBody = '';
-            req.on('data', function (data) {
-                reqBody += data;
-                if (reqBody.length > 1e7) { //10MB
-                    resp.writeHead(413, 'Request Entity Too Large', { 'Content-Type': 'text/html' });
-                    resp.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
-                }
+			collectRequestData(req, result => {
+				console.log(result);
+				res.end(`Book with title '${result.title}' added !`);
             });
-            req.on('end', function () {
-                var pkg = JSON.parse(reqBody);
-                //getCalcForm(req, res, formData);
-                res.writeHead(200);
-                res.end(pkg);
-            });
+
             break
 
         default:
             get404(req, res);
-    }
+	}
+}
+
+function collectRequestData(request, callback) {
+    let body = '';
+    request.on('data', chunk => {
+        body += chunk.toString();
+    });
+    request.on('end', () => {
+        //callback(parse(body));
+        callback(JSON.parse(body));
+    });
 }
 
