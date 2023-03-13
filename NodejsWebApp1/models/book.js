20bab618bcc032eac6c900c76b47dde678ef6068
Razvan Gherman <r.gherman@levi9.com>
2023-03-13 11:20:21 +0200

    Create private field 'title'.

--- NodejsWebApp1/models/book.js
+++ NodejsWebApp1/models/book.js
@@ -1,13 +1,17 @@
 class Book { // Name of the class
+
+    //private field:
+    #title;
+
     // The constructor method will take a number of parameters and assign those parameters as properties to the created object.
     constructor (title, author, year) {
-        this.title = title
+        this.#title = title
         this.author = author
         this.year = year
     }
     // These will be the object's methods.
     fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
-    sayTitle = () => console.log(this.title)
+    sayTitle = () => console.log(this.#title)
 }
 
 module.exports = Book
