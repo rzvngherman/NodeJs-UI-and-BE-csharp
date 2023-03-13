a48cbc3550efc3cc601a6d5f14e9bb376e5c1806
Razvan Gherman <r.gherman@levi9.com>
2023-03-13 14:49:18 +0200

    Refactor.

--- NodejsWebApp1/models/book.js
+++ NodejsWebApp1/models/book.js
@@ -3,17 +3,26 @@ const Author = require('../models/author');
 class Book { // Name of the class
 
     //private field:
-    #title;
+    //#title;
+
+    a = 1;          // .a is public
+    #b = 2;         // .#b is private
+    static #c = 3;  // .#c is private and static
 
     // The constructor method will take a number of parameters and assign those parameters as properties to the created object.
     constructor (title, author, year) {
-        this.#title = title
+        this.title = title
         this.author = new Author(author, "Romania", 1982);
         this.year = year
     }
     // These will be the object's methods.
     fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
-    sayTitle = () => console.log(this.#title)
+    sayTitle = () => console.log(this.title)
+    incB() { this.#b++;}
+
+    get Title() {
+        return this.title;
+      }
 }
 
 module.exports = Book
